623fb8f61593bb124f4ac734c782a15553ba839d
Sinwan Saeed <saeedsinwan@gmail.com>
2025-12-14 13:05:33 -0500

    window open and close events

--- chip8.c
+++ chip8.c
@@ -1,9 +1,155 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <time.h>
 
 #include "SDL.h"
+
+typedef enum {
+    QUIT,
+    RUNNING,
+    PAUSED,
+} emulator_state_t;
+
+typedef struct {
+    emulator_state_t state;
+} chip8_t;
+
+typedef struct {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+} sdl_t;
+
+typedef struct {
+    uint32_t window_width;
+    uint32_t window_height;
+    uint32_t fg_color;
+    uint32_t bg_color;
+    uint32_t scale_factor;
+} config_t;
+
+//Initialize SDL
+bool init_sdl(sdl_t *sdl, config_t *config) {
+    if( SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_TIMER) != 0) {
+        SDL_Log("Unable ot initialize SDL: %s\n", SDL_GetError());
+        return false;
+    }
+
+    sdl->window = SDL_CreateWindow("CHIP8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, config->window_width * config->scale_factor, config->window_height * config->scale_factor,0);
+
+    if (!sdl->window) {
+        SDL_Log("Could not create window: %s\n", SDL_GetError());
+        return false;
+    }
+
+    sdl->renderer = SDL_CreateRenderer(sdl->window, -1, SDL_RENDERER_ACCELERATED);
+    if(!sdl->renderer) {
+        SDL_Log("Could not create renderer: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+//Set up emulator config from passed in args
+bool set_config(config_t *config, const int argc, char **argv) {
+    //defaults
+    config->window_width = 64;
+    config->window_height = 32;
+    config->fg_color = 0xFFFFFFFF;
+    config->bg_color = 0x00000000;
+    config->scale_factor = 20;
+
+    //override defaults with args
+    for (int i = 1; i < argc; i++){
+        (void)argv[i];
+    }
+
+    return true;
+}
+
+bool init_chip8(chip8_t *chip8) {
+    chip8->state = RUNNING;
+    return true;
+}
+
+//Quit SDL
+void quit_sdl(const sdl_t sdl) {
+    SDL_DestroyRenderer(sdl.renderer);
+    SDL_DestroyWindow(sdl.window);
+    SDL_Quit();
+}
+
+void clear_screen(const sdl_t sdl, const config_t config) {
+    const uint8_t r = (config.bg_color >> 24) & 0xFF;
+    const uint8_t g = (config.bg_color >> 16) & 0xFF;
+    const uint8_t b = (config.bg_color >>  8) & 0xFF;
+    const uint8_t a = (config.bg_color >>  0) & 0xFF;
+
+    SDL_SetRenderDrawColor(sdl.renderer, r, g, b, a);
+    SDL_RenderClear(sdl.renderer);
+}
+
+void update_screen(const sdl_t sdl) {
+    SDL_RenderPresent(sdl.renderer);
+}
+
+void handle_input(chip8_t *chip8) {
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                chip8->state = QUIT;
+                break;
+
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_ESCAPE:
+                    chip8->state = QUIT;
+                    break;
+                
+                default:
+                    break;
+                }
+                break;
+
+            case SDL_KEYUP:
+                break;
+
+            default:
+                break;
+
+        }
+    }
+}
 
 int main(int argc, char **argv){
     (void) argc;
     (void) argv;
-    puts("TEST Linux");
+    
+    config_t config;
+    if (!set_config(&config, argc, argv)) exit(EXIT_FAILURE);
+
+    sdl_t sdl;
+    if (!init_sdl(&sdl, &config)) exit(EXIT_FAILURE);
+
+    chip8_t chip8 = {0};
+    if (!init_chip8(&chip8)) exit(EXIT_FAILURE);
+
+    clear_screen(sdl, config);
+
+    while(chip8.state != QUIT) {
+        handle_input(&chip8);
+
+        SDL_Delay(16);
+
+        update_screen(sdl);
+    }
+
+    quit_sdl(sdl);
+
+    exit(EXIT_SUCCESS);
 }
--- makefile
+++ makefile
@@ -1,3 +1,4 @@
+CC = gcc
 CFLAGS=-std=c17 -Wall -Wextra -Werror
 all:
-	gcc chip8.c -o chip8 $(CFLAGS) `sdl2-config --cflags --libs`
+	$(CC) chip8.c -o chip8 $(CFLAGS) `sdl2-config --cflags --libs`
